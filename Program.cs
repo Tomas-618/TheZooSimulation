2d0c635851a394f2afa061198a3c156f2045c68a
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-02-24 13:01:57 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheZooSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
